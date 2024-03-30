1e5edbb5801a61040aeb16a0e895d6f30cd85ab8
Henna Maria <hennamaria2001@gmail.com>
2024-03-30 10:15:20 +0530

    inbulid middleware were added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
+const cors = require("cors")
 
 const app = new express();
+
+app.use(cors({
+    Credential: true,
+    origin: process.env.ORIGIN
+}));
+app.use(express.json({limit: '20kb'}));
+app.use(express.urlencoded({extended: true, limit: "20kb"}));
+app.use(express.static("public"))
+app.use(cookieParser());
 
 module.exports = app;
