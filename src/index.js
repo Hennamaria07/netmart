39b600cc98793f990bbd84c4a9ee1225b21b890f
Henna Maria <hennamaria2001@gmail.com>
2024-03-30 10:09:59 +0530

    dotenv configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const app = require("./app");
+const dotenv = require("dotenv");
 
-const port = 4000;
+dotenv.config({path: "./.env"})
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`server is up at port ${port}`);
